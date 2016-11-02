0e0ca518a685606d637e88892ecbd4d283631275
gTopa <gtopa@stuy.edu>
2016-11-02 12:39:49 -0400

    created mock blog

---
+++ utils/auth.py
@@ -0,0 +1,27 @@
+import hashlib, sqlite3
+
+
+def addUser(user, password):
+    db=sqlite3.connect('../data/tables.db')
+    c=db.cursor()
+    myHashObj=hashlib.sha1()
+    myHashObj.update(password)
+    q='SELECT username FROM users'
+    if(user in c.execute(q)):
+        return False
+    q='SELECT userID FROM users'
+    q='INSERT INTO users ('+str(c.execute(q)[len(c.execute(q))-1]+1)+', '+user+', '+myHashObj.hexdigest()+');'
+    c.execute(q)
+    return True
+
+def userLogin(user, password)
+    db=sqlite3.connect('../data/tables.db')
+    c=db.cursor()
+    myHashObj=hashlib.sha1()
+    myHashObj.update(password)
+    q='SELECT username FROM users'
+    if(user in c.execute(q)):
+        q='SELECT pass FROM users WITH username='+user+';'
+        if(myHashObj.hexdigest()==c.execute(q)[0]):
+            return True
+    return False
